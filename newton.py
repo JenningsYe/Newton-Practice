75d32abdc6b52a1e08739649b53a7d84fc5e3951
Jennings Ye <jennings_ye@berkeley.edu>
2024-08-22 13:21:22 -0700

    Linting and cleanup

--- newton.py
+++ newton.py
@@ -1,16 +1,18 @@
 def optimize(x0, fun):
-    err=1
-    x=x0
-    h=1e-3
-    eps =1e-2;
+    change = 1
+    x = x0
+    h = 1e-3
+    eps = 1e-2
     while abs(change) > 1e-6:
-        xold=x
-        x=x-eps*deriv(x, fun, h)/deriv2(x, fun, h)
-        change = fun(x)-fun(xold)
+        xold = x
+        x = x - eps * deriv(x, fun, h) / deriv2(x, fun, h)
+        change = fun(x) - fun(xold)
     return [x, fun(x)]
-    
+
+
 def deriv(x, fun, h):
-    return (fun(x+h)-fun(x-h))/2/h
-    
+    return (fun(x + h) - fun(x - h)) / 2 / h
+
+
 def deriv2(x, fun, h):
-    return (fun(x+h)-2*fun(x)+fun(x-h))/h**2
+    return (fun(x + h) - 2 * fun(x) + fun(x - h)) / h**2

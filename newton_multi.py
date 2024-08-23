c7c304a59b4e5e1bc1ad0adc292e960672fb504e
Jennings Ye <jennings_ye@berkeley.edu>
2024-08-23 10:06:41 -0700

    Add test function

--- newton_multi.py
+++ newton_multi.py
@@ -1,6 +1,9 @@
 import numpy as np
 import numdifftools as nd
 
+y=np.random.normal(2)
+x=np.random.normal(2)
+
 def optimize(x0, fun):
     change = 1
     x = x0
@@ -12,3 +15,8 @@ def optimize(x0, fun):
         x = x - eps*np.linalg.solve(hess(x), grad(x))
         change = pow(np.sum(x-x_old)**2,0.5)
     return [x, fun(x)]
+
+def test_fun(theta):
+    return np.sum((y - theta[0] - theta[1]*x)**2)
+
+optimize(np.array([2,0]), test_fun)
